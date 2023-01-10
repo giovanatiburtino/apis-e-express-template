a0f635cd26d56e1d68a4c4b88d2927ca3ef75ea2
giova <giovanaftiburtino@gmail.com>
2023-01-09 23:45:05 -0300

    exercicio de manha

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { courses, students } from './database'
+import { TCourse, TStudent } from './types'
 
 const app = express()
 
@@ -12,4 +14,65 @@ app.listen(3003, () => {
 
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
+})
+
+app.get('/courses', (req: Request, res: Response) => {
+    res.status(200).send(courses)
+})
+
+app.get('/courses/search', (req: Request, res: Response) => {
+    const q = req.query.q as string
+
+    const result = courses.filter(
+    (course) => course.name.toLowerCase().includes(q.toLowerCase()))
+
+    res.status(200).send(result)
+})
+
+app.post('/courses', (req: Request, res: Response) => {
+    // const id = req.body.id as string
+    // const name = req.body.name as string
+    // const lessons = req.body.lessons as number
+    // const stack = req.body.stack as COURSE_STACK
+
+    const {id, name, lessons, stack} = req.body as TCourse
+
+    const newCourse = {
+        id: id,
+        name: name,
+        lessons: lessons,
+        stack: stack
+    }
+
+    courses.push(newCourse)
+
+    res.status(201).send("Curso registrado com sucesso")
+})
+
+app.get("/students", (req: Request, res: Response) => {
+    res.status(200).send(students)
+})
+
+app.get("/students/search", (req: Request, res: Response) => {
+    const q = req.query.q as string
+
+    const filteredStudents = students.filter(
+        (student) => student.name === q
+    )
+
+    res.status(200).send(filteredStudents)
+})
+
+app.post('/students', (req: Request, res: Response) => {
+    const { id, name, age } = req.body as TStudent
+
+    const newStudent = {
+        id,
+        name,
+        age
+    }
+
+    students.push(newStudent)
+
+    res.status(201).send("Cadastro do estudante criado com sucesso")
 })
